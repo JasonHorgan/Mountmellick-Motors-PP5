08251c01704c42ac86c11fd712c515f0e7f4fe00
Jason Horgan <jasonahorgan@gmail.com>
2024-12-21 15:44:43 +0000

    Customized admin panel

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import Stock, Reg
 
 # Register your models here.
-admin.site.register(Stock)
-admin.site.register(Reg)
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = (
+        'reg',
+        'make',
+        'model',
+        'price',
+        'description',
+        'image',
+    )
+
+    ordering = ('reg',)
+
+
+admin.site.register(Stock, StockAdmin)
+admin.site.register(Reg,)
 
--- products/models.py
+++ products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Reg(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Registration numbers'
+
     reg_number = models.CharField(max_length=254)
 
     def __str__(self):
