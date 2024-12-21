50e9b1b7d7e9166e2054c9d36621d20349fd6d66
Jason Horgan <jasonahorgan@gmail.com>
2024-12-21 16:01:13 +0000

    Add products views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Stock
 
 # Create your views here.
+
+def all_stock (request):
+    """ A view to show all stock, including sorting and search queries """
+
+    stock = Stock.objects.all()
+
+    context = {
+        'Stock_items': stock,
+    }
+
+    return render(request, 'products/products.html', context)
