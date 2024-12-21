c393312e745ef198390800198d2f7563ded2bb86
Jason Horgan <jasonahorgan@gmail.com>
2024-12-21 18:31:11 +0000

    Add product details functionality

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_stock, name='stock')
+    path('', views.all_stock, name='stock'),
+    path('<stock_id>', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Stock
 
 # Create your views here.
@@ -9,7 +9,18 @@ def all_stock (request):
     stock = Stock.objects.all()
 
     context = {
-        'Stock_items': stock,
+        'stock': stock,
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail (request, stock_id):
+    """ A view to show individual stock details """
+
+    stock = get_object_or_404(Stock, pk=stock_id)
+
+    context = {
+        'stock': stock,
+    }
+
+    return render(request, 'products/product_detail.html', context)
