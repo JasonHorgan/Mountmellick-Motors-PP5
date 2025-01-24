b9eaa58ae41d45ec647366a18d7f0380ae3bba27
Jason Horgan <jasonahorgan@gmail.com>
2025-01-24 18:00:46 +0000

    Add product view, url and template for product admin

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_stock, name='stock'),
-    path('<stock_id>', views.product_detail, name='product_detail'),
+    path('<int:stock_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
