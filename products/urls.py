7764c123ed7695f1a1bf0158b12ca49066776516
Jason Horgan <jasonahorgan@gmail.com>
2025-01-24 19:24:06 +0000

    Add edit product viewn for admins

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.all_stock, name='stock'),
     path('<int:stock_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:stock_id>/', views.edit_product, name='edit_product'),
+    
 ]
