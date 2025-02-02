d9f1d12596fe2a70090110fa562d7ad7a6bc19d1
Jason Horgan <jasonahorgan@gmail.com>
2025-02-02 16:05:39 +0000

    Add test drives and finance applications to admin page

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import FinanceApplication
 
-# Register your models here.
+class FinanceApplicationAdmin(admin.ModelAdmin):
+    list_display = ['application_number', 'user', 'car', 'full_name', 'email', 'loan_term', 'application_date']
+    search_fields = ['full_name', 'email', 'car__make', 'car__model', 'application_number']
+    list_filter = ['loan_term', 'employment_status', 'preferred_contact_method', 'application_date']
+    readonly_fields = ['application_number', 'application_date']
+
+admin.site.register(FinanceApplication, FinanceApplicationAdmin)
--- test_drives/admin.py
+++ test_drives/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import TestDrive
 
-@admin.register(TestDrive)
 class TestDriveAdmin(admin.ModelAdmin):
-    list_display = ('user', 'car', 'date', 'time', 'status', 'created_at')
-    list_filter = ('status', 'date')
-    search_fields = ('user__username', 'car__make', 'car__model')
+    list_display = ['user', 'car', 'date', 'time', 'status', 'created_at']
+    search_fields = ['user__username', 'car__make', 'car__model']
+    list_filter = ['status', 'date']
+    readonly_fields = ['created_at']  
+
+admin.site.register(TestDrive, TestDriveAdmin)
