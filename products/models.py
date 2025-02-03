554fa509aedc16ff23bac444887d9ede7034286f
Jason Horgan <jasonahorgan@gmail.com>
2025-02-03 13:26:08 +0000

    Edit price field in stock model and make migrations

--- products/models.py
+++ products/models.py
@@ -38,7 +38,7 @@ class Stock(models.Model):
         blank=True
     )
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
