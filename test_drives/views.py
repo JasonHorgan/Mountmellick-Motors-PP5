ece42a5b7479ada72425008380dfd3aca95eada6
Jason Horgan <jasonahorgan@gmail.com>
2025-02-04 18:52:08 +0000

    Add ability to edit and delete test drives

--- test_drives/views.py
+++ test_drives/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import TestDriveForm
-
+from .models import TestDrive
 
 @login_required
 def book_test_drive(request):
@@ -17,6 +17,27 @@ def book_test_drive(request):
 
     return render(request, "test_drives/book_test_drive.html", {"form": form})
 
-
 def test_drive_confirmation(request):
     return render(request, "test_drives/confirmation.html")
+
+@login_required
+def edit_test_drive(request, test_drive_id):
+    test_drive = get_object_or_404(TestDrive, id=test_drive_id, user=request.user)
+    if request.method == "POST":
+        form = TestDriveForm(request.POST, instance=test_drive)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = TestDriveForm(instance=test_drive)
+
+    return render(request, "test_drives/edit_test_drive.html", {"form": form, "test_drive": test_drive})
+
+@login_required
+def delete_test_drive(request, test_drive_id):
+    test_drive = get_object_or_404(TestDrive, id=test_drive_id, user=request.user)
+    if request.method == "POST":
+        test_drive.delete()
+        return redirect("profile")
+
+    return render(request, "test_drives/delete_test_drive.html", {"test_drive": test_drive})
